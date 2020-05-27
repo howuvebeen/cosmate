5f70336e4531ad8c13704a01a8d1fddd156f9874
choikh0423 <kyuhwanchoi0423@gmail.com>
2020-05-27 19:50:02 +0900

    Edit admin to have all model instances present

--- backend/reviewweb/reviews/admin.py
+++ backend/reviewweb/reviews/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Company, Ingredient, Product, Review, Like, Feedback, Comment
 
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(Ingredient)
+admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(Like)
+admin.site.register(Feedback)
+admin.site.register(Comment)
--- backend/reviewweb/users/models.py
+++ backend/reviewweb/users/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 import datetime
 
 
