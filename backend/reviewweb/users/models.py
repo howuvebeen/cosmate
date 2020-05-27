f239287e13710978096b6d34e7f704b3a3da7f79
Dongho Kim <dhkim0067@gmail.com>
2020-05-27 20:29:03 +0900

    Updated models in users and reviews

--- backend/reviewweb/users/models.py
+++ backend/reviewweb/users/models.py
@@ -41,3 +41,5 @@ class Profile(models.Model):
         max_length=20, choices=SKINSHADE_CHOICES, default="F", null=True)
     influencer = models.CharField(
         max_length=20, choices=INFLUENCER_CHOICES, default="N", null=True)
+
+
