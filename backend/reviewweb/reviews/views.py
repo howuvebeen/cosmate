b9ae571ab782f3aaaf134ab9561f65c49072668b
choikh0423 <kyuhwanchoi0423@gmail.com>
2020-05-28 19:43:54 +0900

    Implement basic reviews/likes system

--- backend/reviewweb/reviews/views.py
+++ backend/reviewweb/reviews/views.py
@@ -4,12 +4,14 @@ from rest_framework import generics
 from reviews.models import Review, Like
 from reviews.serializers import ReviewSerializer, LikeSerializer
 from users.models import Profile
+from django.contrib.auth.models import User
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 from django.shortcuts import get_object_or_404
+
 
 @api_view(['GET'])
 def api_root(request, format=None):
@@ -22,12 +24,6 @@ class ReviewList(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
-    def perform_create(self, serializer):
-
-        profile = get_object_or_404(Profile, user = self.request.user.pk)
-
-        serializer.save(author=profile)
-
 
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
@@ -37,9 +33,6 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
 class LikeList(generics.ListCreateAPIView):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.profile)
 
 
 class LikeDetail(generics.RetrieveUpdateDestroyAPIView):
--- backend/reviewweb/users/models.py
+++ backend/reviewweb/users/models.py
@@ -42,7 +42,7 @@ class Profile(models.Model):
         max_length=20, choices=SKINSHADE_CHOICES, default="F", null=True)
     influencer = models.CharField(
         max_length=20, choices=INFLUENCER_CHOICES, default="N", null=True)
-    
+
     def __str__(self):
         return self.user.first_name
 
