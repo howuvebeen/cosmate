a5822aa5faac86456fe239409bc56108f0bb6aa5
Dongho Kim <dhkim0067@gmail.com>
2020-05-29 16:06:28 +0900

    Fix Review Like counting

--- backend/reviewweb/reviews/serializers.py
+++ backend/reviewweb/reviews/serializers.py
@@ -13,6 +13,15 @@ class ReviewSerializer(serializers.ModelSerializer):
 
     likes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
+    like_number = serializers.SerializerMethodField()
+
+    def get_like_number(self, obj):
+        likelist = list(Like.objects.filter(review=obj))
+        for like in likelist:
+            obj.like_number += 1
+
+        return obj.like_number
+
     class Meta:
         model = Review
         read_only_fields = ['pub_date']
