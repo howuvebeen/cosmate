364ece4fbdb5925cdddf597c0f6a32c1f5b97459
choikh0423 <kyuhwanchoi0423@gmail.com>
2020-05-30 14:33:27 +0900

    initialized likes, but has an error in signaling

--- backend/reviewweb/reviews/serializers.py
+++ backend/reviewweb/reviews/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
 from .models import Review, Like, Feedback
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class LikeSerializer(serializers.ModelSerializer):
@@ -16,16 +18,21 @@ class ReviewSerializer(serializers.ModelSerializer):
     like_number = serializers.SerializerMethodField()
 
     def get_like_number(self, obj):
-        likelist = list(Like.objects.filter(review=obj))
-        likenum = 0
-        for like in likelist:
-            likenum += 1
-
-        if likenum > obj.like_number:
-            obj.like_number = likenum
-
-        obj.save()
         return obj.like_number
+
+    # @receiver(post_save, sender=Like)
+    # def get_like_number(sender, instance, **kwargs):
+    #     review = instance.review
+    #     likelist = list(Like.objects.filter(review=review))
+    #     likenum = 0
+    #     for like in likelist:
+    #         likenum += 1
+
+    #     if likenum > review.like_number:
+    #         review.like_number = likenum
+
+    #     review.save()
+    #     return review.like_number
 
     class Meta:
         model = Review
