2adcb17109add8f5896c5fb9508a6ef56b41578d
choikh0423 <kyuhwanchoi0423@gmail.com>
2020-06-06 16:10:26 +0900

    Implement firstname, lastname, email onto profile serializers

--- backend/reviewweb/users/serializers.py
+++ backend/reviewweb/users/serializers.py
@@ -29,18 +29,34 @@ class ProfileSerializer(serializers.ModelSerializer):
         choices=SKINISSUE_CHOICES, default='N/A')
     interested_product = ProductSerializer(many=True, required=False)
     user = serializers.StringRelatedField(read_only=True)
+    firstname = serializers.SerializerMethodField()
+    lastname = serializers.SerializerMethodField()
+    email = serializers.SerializerMethodField()
     last_login = serializers.SerializerMethodField()
 
     def get_last_login(self, obj):
         last_login = obj.user.last_login
         return last_login
 
+    def get_firstname(self, obj):
+        firstname = obj.user.first_name
+        return firstname
+
+    def get_lastname(self, obj):
+        lastname = obj.user.last_name
+        return lastname
+
+    def get_email(self, obj):
+        email = obj.user.email
+        return email
+
     class Meta:
         model = Profile
         read_only_fields = ['user', 'interested_product',
                             'last_login', 'age_range']
-        fields = ['user', 'gender', 'dob', 'age', 'skintype', 'skinissue',
-                  'influencer', 'interested_product', 'age_range', 'last_login']
+        fields = ['user', 'firstname', 'lastname', 'email', 'last_login',
+                  'gender', 'dob', 'age', 'skintype', 'skinissue', 'influencer',
+                  'interested_product', 'age_range']
 
 
 class TokenSerializer(serializers.ModelSerializer):
